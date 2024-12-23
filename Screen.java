c52254deead26dbe5d5c450d2b44869d92a17735
kyiwsr <waseemirham07@gmail.com>
2024-12-23 23:51:04 +0800

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -2,5 +2,17 @@ import javax.swing.*;
 
 public class Screen {
 
+    private JTextField screen;
+
+    public Screen(){
+        screen = new JTextField();
+    }
     
+    public void screenAttributes(){
+        screen.setBounds(10, 10, 376, 170);
+    }
+
+    public JTextField getscreen(){
+        return screen;
+    }
 }
